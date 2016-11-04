1f80cb2638607f19f819ad151cd59bbcda216c69
Thibault KUMMER <tkummer@quechoisir.org>
2016-11-04 10:11:37 +0100

    Workflow import

--- list-workflows.php
+++ list-workflows.php
@@ -34,16 +34,47 @@ $xsl = new XSLEngine();
 
 
 // INSTALL WORKFLOW
-if (isset($_FILES['workflow_zip_file'])) {
-	$errors = Workflow::Import($_FILES['workflow_zip_file']['tmp_name']);
-	if ($errors === true)
-	{
-		$xsl->AddNotice('Installed workflow successfully!');
-		WorkflowInstance::SyncTasks();
+try
+{
+	if (isset($_FILES['workflow_zip_file'])) {
+		$zip = new ZipArchive();
+		if($zip->open($_FILES['workflow_zip_file']['tmp_name'])!==true)
+		{
+			$xsl->AddError('Uploaded file is not a valid workflow archive');
+			throw new Exception();
+		}
+		
+		$workflow_xml = $zip->getFromName('workflow.xml');
+		
+		$workflow_dom = new DOMDocument();
+		$workflow_dom->loadXML($workflow_xml);
+		
+		$attributes = [];
+		foreach($workflow_dom->documentElement->attributes as $attribute)
+			$attributes[$attribute->name] = $attribute->value;
+		
+		$attributes['content'] = base64_encode($workflow_dom->saveXML($workflow_dom->documentElement->firstChild));
+		
+		$xsl->Api('workflow','create',$attributes);
+		
+		$workflow_xpath = new DOMXPath($workflow_dom);
+		$tasks = $workflow_xpath->query('//task');
+		foreach($tasks as $task)
+		{
+			$task_xml = $zip->getFromName('tasks/'.$task->getAttribute('name').'.xml');
+			
+			$task_dom = new DOMDocument();
+			$task_dom->loadXML($task_xml);
+			
+			$attributes = [];
+			foreach($task_dom->documentElement->attributes as $attribute)
+				$attributes[$attribute->name] = $attribute->value;
+			
+			$xsl->Api('task','create',$attributes);
+		}
 	}
-	else
-		$xsl->AddErrors($errors);
 }
+catch(Exception $e) {}
 
 $xsl->AddFragment(["workflows" => $xsl->Api("workflows", "list")]);
 
