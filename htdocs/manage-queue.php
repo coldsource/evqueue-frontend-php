39805ba208fea78a4ed36668a736f5dcb545be80
www-data <www-data@srvdev.qc.ufc>
2017-04-10 11:00:59 +0200

    edit dynamic queue

--- htdocs/manage-queue.php
+++ htdocs/manage-queue.php
@@ -1,12 +1,12 @@
 <?php
  /*
   * This file is part of evQueue
-  * 
+  *
   * evQueue is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 3 of the License, or
   * (at your option) any later version.
-  * 
+  *
   * evQueue is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -14,8 +14,8 @@
   *
   * You should have received a copy of the GNU General Public License
   * along with evQueue. If not, see <http://www.gnu.org/licenses/>.
-  * 
-  * Authors: Nicolas Jean, Christophe Marti 
+  *
+  * Authors: Nicolas Jean, Christophe Marti
   */
 
 require_once 'inc/auth_check.php';
@@ -30,10 +30,21 @@ $xsl = new XSLEngine();
 $xml_error = "";
 if (isset($_POST) && (count($_POST)>1)){
 	if(!$_POST['queue_id'])
-		$xsl->Api('queue','create',['name' => $_POST['queue_name'], 'concurrency' => $_POST['queue_concurrency'], 'scheduler' => $_POST['queue_scheduler']]);
+		$xsl->Api('queue','create',[
+        'name' => $_POST['queue_name'],
+        'concurrency' => $_POST['queue_concurrency'],
+        'dynamic' => ($_POST['dynamic']=='1' ? 'yes' : 'no'),
+        'scheduler' => $_POST['queue_scheduler']
+      ]);
 	else
-		$xsl->Api('queue','edit',['id' => $_POST['queue_id'], 'name' => $_POST['queue_name'], 'concurrency' => $_POST['queue_concurrency'], 'scheduler' => $_POST['queue_scheduler']]);
-	
+		$xsl->Api('queue','edit',[
+        'id' => $_POST['queue_id'],
+        'name' => $_POST['queue_name'],
+        'concurrency' => $_POST['queue_concurrency'],
+        'dynamic' => ($_POST['dynamic']=='1' ? 'yes' : 'no'),
+        'scheduler' => $_POST['queue_scheduler']
+      ]);
+
 	if (!$xsl->HasError()){
 		header("location:list-queues.php");
 		die();
