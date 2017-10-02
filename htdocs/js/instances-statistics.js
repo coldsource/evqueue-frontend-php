6768d9fed2360bff5ead50684c3eca35d497e61e
Thibault KUMMER <tkummer@quechoisir.org>
2017-10-02 14:11:01 +0200

    Added instances statistics pages

---
+++ htdocs/js/instances-statistics.js
@@ -0,0 +1,169 @@
+var groupby = 'day';
+var statistics_data_dom = false;
+var nodes = false;
+
+$(document).ready(function() {
+	jQuery.getJSON('ajax/get-nodes.php',function(nodes_local) {
+		nodes = nodes_local;
+		
+		evqueueAPI({
+			group: 'instances',
+			action: 'list',
+			attributes: {'groupby': groupby, 'limit':10000}
+		}).done(function(xml) {
+			statistics_data_dom = xml;
+			
+			
+			$('.graph-container').each(function() {
+				refresh_graph($(this));
+			});
+		});
+	});
+	
+	$('.graph-container').delegate('input','change',function() {
+		refresh_graph($(this).parents('.graph-container'));
+	});
+});
+
+function refresh_graph(graph_container)
+{
+	var group = graph_container.data('group');
+	
+	var include = [':all'];
+	var filter = [];
+	graph_container.find('.graph-workflows input').each(function() {
+		filter.push($(this).attr('name'));
+		if($(this).prop('checked'))
+			include.push($(this).attr('name'));
+	});
+	
+	var counts = {};
+	var workflow_nodes = statistics_data_dom.Query('workflow',statistics_data_dom.documentElement);
+	for(var i=0;i<workflow_nodes.length;i++)
+	{
+		var name = workflow_nodes[i].getAttribute('name');
+		if(group!=':all' && jQuery.inArray(name, filter)==-1)
+			continue;
+		
+		var x = workflow_nodes[i].getAttribute('year');
+		if(workflow_nodes[i].hasAttribute('month'))
+			x += workflow_nodes[i].getAttribute('month');
+		if(workflow_nodes[i].hasAttribute('day'))
+			x += workflow_nodes[i].getAttribute('day');
+		if(workflow_nodes[i].hasAttribute('hour'))
+			x += workflow_nodes[i].getAttribute('hour');
+		
+		var node_name = workflow_nodes[i].getAttribute('node_name');
+		var count = parseInt(workflow_nodes[i].getAttribute('count'));
+		
+		if(!counts.hasOwnProperty(x))
+			counts[x] = {};
+		
+		if(!counts[x].hasOwnProperty(':all'))
+			counts[x][':all'] = 0;
+		
+		if(!counts[x].hasOwnProperty(name))
+			counts[x][name] = 0;
+		
+		if(!counts[x].hasOwnProperty('@'+node_name))
+		{
+			counts[x]['@'+node_name] = {};
+			counts[x]['@'+node_name][':all'] = 0;
+		}
+		
+		counts[x]['@'+node_name][name] =count;
+		counts[x]['@'+node_name][':all'] +=count;
+		counts[x][name] += count;
+		counts[x][':all'] += count;
+	}
+	
+	var nit = 10;
+	var current_date = new Date();
+	
+	var graph_data = [];
+	var graph_counts = [];
+	for(var i=0;i<nit;i++)
+	{
+		var x = "" + current_date.getFullYear();
+		var x_human = "" + current_date.getFullYear();
+		if(groupby=='month' || groupby=='day' || groupby=='hour')
+		{
+			x += (current_date.getMonth()+1);
+			x_human += "-"+(current_date.getMonth()+1);
+		}
+		if(groupby=='day' || groupby=='hour')
+		{
+			x += current_date.getDate();
+			x_human += "-"+current_date.getDate();
+		}
+		if(groupby=='hour')
+		{
+			x += current_date.getHours();
+			x_human += " "+current_date.getHours()+":00:00";
+		}
+		
+		var point = {_t:x_human};
+		for(var j=0;j<include.length;j++)
+		{
+			point[include[j]] = 0;
+			if(counts.hasOwnProperty(x) && counts[x].hasOwnProperty(include[j]))
+				point[include[j]] = counts[x][include[j]];
+		}
+
+		graph_data.push(point);
+		graph_counts.push(counts.hasOwnProperty(x)?counts[x]:false);
+		
+		if(groupby=='hour')
+			current_date.setHours(current_date.getHours()-1);
+		else if(groupby=='day')
+			current_date.setHours(current_date.getHours()-24);
+		else if(groupby=='month')
+			current_date.setMonth(current_date.getMonth()-1);
+		else if(groupby=='year')
+			current_date.setMonth(current_date.getMonth()-12);
+		
+	}
+	
+	var graph_id = group!=':all'?'graph-'+group:'graph';
+	$('#'+graph_id).empty();
+	new Morris.Line({
+		element: graph_id,
+		data: graph_data,
+		xkey: '_t',
+		ykeys: include,
+		labels: include,
+		hoverCallback: function(index, options, content) {
+			var has_elements = false;
+			var counts = graph_counts[graph_data.length-index-1];
+			var desc = '<div style="width:200px;">';
+			for(var i=0;i<include.length;i++)
+			{
+				var wf = include[i];
+				
+				if(counts.hasOwnProperty(wf))
+				{
+					has_elements = true;
+					var name = wf==':all'?'All workflows':wf;
+					desc += "<div style='font-weight:bold;color:"+options.lineColors[i]+"'>"+name+" (total : "+((counts!==false && counts.hasOwnProperty(wf))?counts[wf]:0)+")</div>";
+					if(counts!==false)
+					{
+						desc += "<ul>";
+						for(var j=0;j<nodes.nodes.length;j++)
+						{
+							var node = nodes.nodes[j];
+							if(counts.hasOwnProperty('@'+node) && counts['@'+node].hasOwnProperty(wf))
+								desc += "<li>"+node+" : "+counts['@'+node][wf]+"</li>";;
+						}
+						desc += "</ul>";
+					}
+				}
+			}
+			
+			if(!has_elements)
+				desc += "No instances launched";
+			
+			desc += "</div>";
+			return desc;
+		},
+	});
+}
---
+++ htdocs/xsl/instances-statistics.xsl
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:import href="templates/main-template.xsl" />
+	<xsl:import href="templates/git.xsl" />
+
+	<xsl:variable name="topmenu" select="'settings'" />
+	
+	<xsl:variable name="javascript">
+		<src>js/morris/morris.min.js</src>
+		<src>js/morris/raphael.min.js</src>
+		<src>js/instances-statistics.js</src>
+	</xsl:variable>
+	
+	<xsl:variable name="css">
+		<src>styles/morris/morris.css</src>
+	</xsl:variable>
+	
+	<xsl:key name="groups" match="/page/workflows/workflow/@group" use="." />
+
+	<xsl:template name="content">
+		<h2>General summary</h2>
+		<div class="graph-container" data-group=":all">
+			<div class="graph" id="graph"></div>
+		</div>
+		
+		<xsl:for-each select="/page/workflows/workflow/@group[generate-id(.) = generate-id(key('groups', .))]">
+			<xsl:sort select="." />
+			<xsl:variable name="groupName" select="." />
+			
+			<br /><br />
+			<xsl:choose>
+				<xsl:when test="$groupName != ''">
+					<h2><xsl:value-of select="$groupName" /></h2>
+				</xsl:when>
+				<xsl:otherwise>
+					<h2>No group</h2>
+				</xsl:otherwise>
+			</xsl:choose>
+			
+			<div class="graph-container" data-group="{$groupName}">
+				<div class="graph" id="graph-{$groupName}"></div>
+				<div class="graph-workflows">
+					<xsl:for-each select="/page/workflows/workflow[@group = $groupName]">
+						<div class="graph-workflow">
+							<input type="checkbox" name="{@name}" />
+							<label><xsl:value-of select="@name" /></label>
+						</div>
+					</xsl:for-each>
+				</div>
+			</div>
+		</xsl:for-each>
+	</xsl:template>
+
+</xsl:stylesheet>
